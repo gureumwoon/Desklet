5c08edbf989cff2ca53b1854e121c6719f30dd41
nujeyh <dza456@naver.com>
2022-06-11 14:20:38 +0900

    라우팅 경로 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Signup from "./pages/Signup";
+import Main from "./pages/Main";
+import Detail from "./pages/Detail";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getPosts } from "./redux/modules/postSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts);
+  });
   return (
     <div className="App">
+      <Header />
       <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/posts/:id" element={<Detail />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" />
       </Routes>
+      <Footer />
     </div>
   );
 }
