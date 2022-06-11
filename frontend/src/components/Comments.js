0cffdedfbde7cc4dd884f60bd336cf536eca7dd4
nujeyh <dza456@naver.com>
2022-06-11 14:07:40 +0900

    Comments.js 생성

---
+++ frontend/src/components/Comments.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Comments = () => {
+  return (
+    <div>
+      <span>{"nickname"}</span>
+      <span>{"comments"}</span>
+    </div>
+  );
+};
+
+export default Comments;
